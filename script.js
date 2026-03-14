45994b2a6a65480a60ef1a96d12a43e52b235ca3
Alipovid <deivi.ap.1141@gmail.com>
2026-03-14 15:54:26 -0400

    version final

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const btnProyectos = document.getElementById("ver-proyectos");
-
-btnProyectos.addEventListener("click", function(){
-    document.getElementById("proyectos").scrollIntoView({
-        behavior: "smooth"
-    });
-});
+/* CAMBIAR TEMA */
 
 const btnTema = document.getElementById("btn-tema");
 
 btnTema.addEventListener("click", function(){
-    document.body.classList.toggle("dark-mode");
+
+document.body.classList.toggle("tema-negro");
+
 });
 
+
+/* SCROLL A PROYECTOS */
+
+const btnProyectos = document.getElementById("ver-proyectos");
+
+btnProyectos.addEventListener("click", function(){
+
+document.getElementById("proyectos").scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+
+/* BOTON SUBIR ARRIBA */
+
+const btnArriba = document.getElementById("btn-arriba");
+
+btnArriba.addEventListener("click", function(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+});
